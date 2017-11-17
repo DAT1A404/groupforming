486cb8810a26688a5e70f170d5d5f83b5b43343f
Hindsgaul <mhinds17@student.aau.dk>
2017-11-17 11:07:23 +0100

    added structs for person and group

--- datastructs.c
+++ datastructs.c
@@ -1,1 +1,11 @@
-BOF
+typedef struct person{
+    char name[40];
+    int personID;
+    double criteria[5];
+}person;
+
+typedef struct group{
+    person;
+    int groupNumber;
+    double fitnessValue;
+}group;
