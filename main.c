a1e09d0787152931b2c647324ff258f8d1063a85
cogitantium <falke.cs@gmail.com>
2017-11-17 10:37:03 +0100

    implemented inclusion of separate files in main.c

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /* holds structs for the whole program */
-#include <datastruct.c>
+#include <datastructs.c>
 
 /* holds void read_control() and read_data(FILE *f) */
 #include <read.c>
@@ -10,10 +10,22 @@
 /* holds functions for genetic algorithm */
 #include <genetic.c>
 
+/* holds functions for print, export an exit-commands */
+#include <commands.c>
+
+/* holds functions that print person, persons and groups nicely */
+#include <visual.c>
+
+/* holds functions that export generated data into different file-formats */
+#include <export.c>
+
+/* holds functions that aren't applicable to above categories */
+#include <utility.c>
+
 int main(void) {
-    
+
     /* Read.read_datafile(); */
     genetic_setup();
-    
+
     return EXIT_SUCCESS;
 }
