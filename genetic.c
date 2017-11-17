215d48fd0e33324620966c15fec3b4f76d38ec52
cogitantium <falke.cs@gmail.com>
2017-11-17 14:07:29 +0100

    commented strategy for implementing pseudo-random generator

--- genetic.c
+++ genetic.c
@@ -6,55 +6,61 @@
     - murationrate  : how likely a mutations is to happen. Between [0..1]
 */
 void genetic_algorithm(person **all, int personCount, int popsize, int generations, double mutationrate) {
-    
+
     int gen;
-    
+
     /* Setting up multi-dimensional array of pointers to persons.
         This way no unnersasary data is copied. It's all pointers, baby */
     person ***population genetic_generate_initial_population(all, personCount, popsize);
-    
+
     for (gen = 0; gen < generations; gen++) {
-        
+
         /* Algorithm ... */
     }
-    
+
     free(*population); /* Pointer to the array of memberpointers */
     free(population); /* Pointer to the array of pointers, that points at array of memberpointers */
 }
 
 person*** genetic_generate_initial_population(person **all, int personCount, int popsize) {
-    
+
     int i;
-    
+
     /* Create an array of pointers to persons,
         AND an array of pointers to parts of that other array */
     person *chromosomes = (person*)malloc(personCount * popsize * sizeof(person));
     person **population = (person**)malloc(popsize * sizeof(person*));
-    
+
     for (i = 0; i < popsize; i++) {
         /* Setup populations pointers to the right spots.
             They shall not be moved again. Only their content should change */
         population[i] = chromosomes + i * personCount;
-        
+
         /* Generate chromosome */
         genetic_generate_chromosome(population[i], all, personCount);
     }
-    
+
     return population;
 }
 
 /* Put members randomly into the chromosome array */
 void genetic_generate_chromosome(person **chromosome, person **all, int personCount) {
-    
+
     /* g_rand() returns an array with all integers between 0 and the argument,
         where each values occurs exactly once. */
     int *idx = g_rand(personCount);
-    
-    
-    
+
+
+
     free(idx);
 }
 
 int * g_rand(personCount) {
-    
+  /*
+  need seed generated from srand()
+  could probably use recursion?
+  choose a, c, m, so that we'll get a unique string of integers between 0 and m-1
+  x_n+1 = (ax_n + c) mod m
+  */
+
 }
