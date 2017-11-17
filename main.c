509448b7141bb73d289897fdad99b118a95e8bfc
cogitantium <falke.cs@gmail.com>
2017-11-17 10:24:54 +0100

    added include statements for datastruct, read and genetic c-files

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* holds structs for the whole program */
+#include <datastruct.c>
+
+/* holds void read_control() and read_data(FILE *f) */
+#include <read.c>
+
+/* holds functions for genetic algorithm */
+#include <genetic.c>
+
+int main(void) {
+  return EXIT_SUCCESS;
+}
