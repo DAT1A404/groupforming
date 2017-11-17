16355eb4639fd4d577f0c92e35a4ca3961ac5b02
NicEastvillage <nicoesterby@gmail.com>
2017-11-17 09:46:11 +0100

    added something to main.c

---
+++ main.c
@@ -0,0 +1,16 @@
+
+int main(void) {
+    
+    /* Read.read_datafile(); */
+    
+    genetic_setup();
+    
+    
+    
+    return 0;
+}
+
+void genetic_setup() {
+    
+    /* Genetic.genetic(pop, popsize, generations, mutationrate); */
+}
