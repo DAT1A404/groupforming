b48009cfaf4711f8c8f293370bcedde37c47d6b7
NicEastvillage <nicoesterby@gmail.com>
2017-11-18 12:09:06 +0100

    added function that allocate memory to populations

--- genetic.c
+++ genetic.c
@@ -10,8 +10,12 @@ void genetic_algorithm(person **all, int personCount, int popsize, int generatio
     int gen;
 
     /* Setting up multi-dimensional array of pointers to persons.
-        This way no unnersasary data is copied. It's all pointers, baby */
-    person ***population genetic_generate_initial_population(all, personCount, popsize);
+        This way no unnersasary data is copied. It's all pointers, baby.
+        Also generates the some random chromosomes */
+    person ***population = genetic_generate_initial_population(all, personCount, popsize);
+    
+    /* Similarly, allocate memory to generate a new population */
+    person ***nextGeneration = genetic_get_memory_for_pop(personCount, popsize);
 
     for (gen = 0; gen < generations; gen++) {
         int i;
@@ -72,8 +76,7 @@ void genetic_mutation(person ***child, double mutationrate) {
     /* TODO: Small chance of mutation. Should probably swap to elements at random */
 }
 
-person*** genetic_generate_initial_population(person **all, int personCount, int popsize) {
-
+person*** genetic_get_memory_for_pop(int personCount, int popsize) {
     int i;
 
     /* Create an array of pointers to persons,
@@ -85,8 +88,20 @@ person*** genetic_generate_initial_population(person **all, int personCount, int
         /* Setup populations pointers to the right spots.
             They shall not be moved again. Only their content should change */
         population[i] = chromosomes + i * personCount;
+    }
+    
+    return population;
+}
 
-        /* Generate chromosome */
+person*** genetic_generate_initial_population(person **all, int personCount, int popsize) {
+
+    int i;
+
+    /* Allocate memory to the population */
+    person ***population = genetic_get_memory_for_pop(personCount, popsize);
+
+    /* Generate chromosomes */
+    for (i = 0; i < popsize; i++) {    
         genetic_generate_chromosome(population[i], all, personCount);
     }
 
