98793be559c9c81709562b91d1ebfd5b96ae1266
NicEastvillage <nicoesterby@gmail.com>
2017-11-19 13:31:20 +0100

    now includes correct header files

--- main.c
+++ main.c
@@ -2,25 +2,25 @@
 #include <stdlib.h>
 
 /* holds structs for the whole program */
-#include <datastructs.c>
+#include "datastructs.h"
 
 /* holds void read_control() and read_data(FILE *f) */
-#include <read.c>
+#include "read.h"
 
 /* holds functions for genetic algorithm */
-#include <genetic.c>
+#include "genetic.h"
 
 /* holds functions for print, export an exit-commands */
-#include <commands.c>
+#include "commands.h"
 
 /* holds functions that print person, persons and groups nicely */
-#include <visual.c>
+#include "visual.h"
 
 /* holds functions that export generated data into different file-formats */
-#include <export.c>
+#include "export.h"
 
 /* holds functions that aren't applicable to above categories */
-#include <utility.c>
+#include "utility.h"
 
 int main(void) {
 
