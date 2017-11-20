df08e290d53e062233a0c7cf7ae6389e32f587af
NicEastvillage <nicoesterby@gmail.com>
2017-11-20 13:51:05 +0100

    added posibility to print in colors. #fab

--- visual.c
+++ visual.c
@@ -1,1 +1,32 @@
-BOF
+
+#include <windows.h>
+
+/* Color constants */
+#define BLACK           0
+#define BLUE            1
+#define GREEN           2
+#define CYAN            3
+#define RED             4
+#define MAGENTA         5
+#define BROWN           6
+#define LIGHTGRAY       7
+#define DARKGRAY        8
+#define LIGHTBLUE       9
+#define LIGHTGREEN      10
+#define LIGHTCYAN       11
+#define LIGHTRED        12
+#define LIGHTMAGENTA    13
+#define YELLOW          14
+#define WHITE           15
+
+/* Set the terminals printing colors. Remember to reset */
+void set_color(int ForgC, int BackC)
+{
+    WORD wColor = ((BackC & 0x0F) << 4) + (ForgC & 0x0F);
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), wColor);
+}
+
+/* Reset terminals printing colors */
+void reset_color() {
+    set_color(LIGHTGRAY, BLACK);
+}
