c733810454b9d0e38f8dc4f7f44cd30e29381e3f
NicEastvillage <nicoesterby@gmail.com>
2017-11-30 12:25:27 +0100

    print group now shows group fitness

--- visual.c
+++ visual.c
@@ -30,7 +30,7 @@ void print_all_groups(group *groups, int groupCount) {
 /* Print a group and it's members */
 void print_group(group *g) {
     int i;
-    printf("Gruppe %d:\n", g->groupNumber);
+    printf("Gruppe %d (f = %.2lf):\n", g->groupNumber, g->fitnessValue);
     for (i = 0; i < g->memberCount; i++) {
         printf("%s\n", g->members[i].name);
     }
